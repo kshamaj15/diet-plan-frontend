1952e6779e5f5fa34451e5516cff1a9eb799e00a
kshamaj15 <kshamaj150896@gmail.com>
2020-12-17 18:26:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+
+import Articles from './Pages/Articles/Articles';
+import logo from './Assets/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}/>
+      </header>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Articles/>
+        </Route>
+        <Route path="/diet-plan">
+          <p>hdsa</p>
+        </Route>
+        <Route path="/exercise">
+          <h1>Simmy</h1>
+        </Route>
+      </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Articles/Articles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Articles = props => {
+    return (
+        <div>Articles</div>
+    )
+}
+
+export default Articles;
