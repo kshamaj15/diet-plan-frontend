8e4e17a421911d45af09d176fabcd8bbe2ec715f
kshamaj15 <kshamaj150896@gmail.com>
2020-12-23 22:22:41 +0530

    conditional style added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../Assets/logo.png';
+import cx from 'classnames';
 
 const Header = () => {
+  const [tabname, setTabName] = React.useState(window.location.pathname);
     return (
-      <header className="app-header">
+      <header className={cx("app-header", {
+        "article-header": tabname === '/',
+        "diet-plan-header": tabname === '/diet-plan',
+        "exercise-header": tabname === '/exercise'
+      })}>
         <nav className="app-header-menu">
           <span className="app-header-img"><img src={logo}/></span>
-          <ul>
+          <ul onClick={() => setTabName(window.location.pathname)}>
               <li><NavLink className="link" exact to="/" replace>Articles</NavLink></li>
               <li><NavLink className="link" to="/diet-plan" replace>Diet Plan</NavLink></li>
               <li><NavLink className="link" to="/exercise" replace>Exercise</NavLink></li>
