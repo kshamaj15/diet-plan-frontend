fb9d529e58ac9475530b8693babceb401f8331f2
kshamaj15 <kshamaj150896@gmail.com>
2020-12-23 22:45:29 +0530

    article page started

--- src/Pages/Articles/Articles.js
+++ src/Pages/Articles/Articles.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 const Articles = props => {
     return (
-        <div>Articles</div>
+        <div>
+            <div className="search-bar">
+                <label>
+                    <input type="text" placeholder="Search"/>
+                </label>
+            </div>
+        </div>
     )
 }
 
